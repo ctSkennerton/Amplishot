09d35f8a636fe0ea47b8b1b1a31e5f64591754a0
ctSkennerton <c.skennerton@gmail.com>
2013-01-09 14:05:09 +1000

    added in conserved sequences as markers for partitioning

--- amplishot.py
+++ amplishot.py
@@ -34,6 +34,10 @@ __status__ = "Development"
 import argparse
 import sys
 from pandaseq import Pandaseq
+from cogent import DNA, LoadSeqs
+import cogent.core.moltype
+from cogent.parse.fastq import MinimalFastqParser
+
 #import os
 #import errno
 
@@ -51,8 +55,6 @@ from pandaseq import Pandaseq
 ###############################################################################
 ###############################################################################
 
-  # classes here
-
 ###############################################################################
 ###############################################################################
 ###############################################################################
@@ -64,18 +66,83 @@ def panda(args):
     panda.Parameters['-f'].on(args.read_1)  
     panda.Parameters['-r'].on(args.read_2)
     return panda()
+
+def disambiguate(sequence):
+    index = sequence.firstDegenerate()
+    if index is None:
+        return sequence
+
+    head = sequence[:index]
+    tail = sequence[index+1:]
+    ret_seqs = list()
+
+    for unambiguous in cogent.core.moltype.IUPAC_DNA_ambiguities[sequence[index]]:
+        ret = disambiguate(head+unambiguous+tail)
+        if isinstance(ret, list):
+            ret_seqs.extend(ret)
+        else:
+            ret_seqs.append(ret)
+    return ret_seqs
+
+def generate_unambiguous_sequences():
+    _CONSERVED_SEQUENCES = {
+        '357': 'CTGAGAYACGGHCCARACTCCTACGGGAGGCAGCAG',
+        '530': 'GGCTAAYTHYGTGCCAGCAGCCGCGGTAAKAC',
+        '787': 'CRAACRGGATTAGATACCCYGGTAGTCCW',
+        '926': 'AAACTYAAAKGAATTGRCGG',
+        '1114': 'GGTGSTGCATGGYTGTCGTCAGCTCGTGYCGTGA',
+        '1392': 'GGTGAATACGTTCCCGGGYCTTGYACNCAC'
+        }
+
+    unambiguous_conserved_sequences = list()
+    rev_unambiguous_conserved_sequences = list()
+    for seq in _CONSERVED_SEQUENCES.values():
+        dnaseq = DNA.makeSequence(seq)
+        #print dnaseq, dnaseq.__class__
+        ret = disambiguate(dnaseq)
+        if isinstance(ret, list):
+            unambiguous_conserved_sequences.extend(ret)
+        else:
+            unambiguous_conserved_sequences.append(ret)
+
+    for i in unambiguous_conserved_sequences:
+        rev_unambiguous_conserved_sequences.append(i.rc())
+
+    unambiguous_conserved_sequences.extend(rev_unambiguous_conserved_sequences)
     
+    return unambiguous_conserved_sequences
+
 def doWork( args ):
     """ Main wrapper"""
 
     # generate fragment consensus sequences - pandaseq
-    panda_out = panda(args)
+    #panda_out = panda(args)
 
-    # dataset reduction - velvet or cd-hit
+    # dataset partitioning based on conserved primer presence
+    seqs = generate_unambiguous_sequences()
+    for i in seqs:
+        tree.add(str(i))
+    tree.make()
+    count = 0
+    with open(args.infile) as fp:
+        for name, seq, qual in MinimalFastqParser(fp):
+            pass
+    print count
+    # dataset reduction - cd-hit - each partition - threaded
 
-    # generate overlaps - phrap
+    # generate overlaps - phrap - each partition - threaded
 
-    #read alignment - bwa or bowtie
+    #combined assemblies - phrap
+
+    #chimera checking - uchime, decipher, chimeraslayer
+
+    #read alignment - bwa
+
+    #generate abundance based on coverage
+
+    #assign taxonomy
+
+    #generte OTU table
 
     return 0
 
@@ -90,13 +157,14 @@ if __name__ == '__main__':
     #parser.add_argument('positional_arg', help="Required")
     #parser.add_argument('positional_arg2', type=int, help="Integer argument")
     #parser.add_argument('positional_arg3', nargs='+', help="Multiple values")
-    #parser.add_argument('-X', '--optional_X', action="store_true", default=False, help="flag")
+    parser.add_argument('-i', '--infile', dest='infile', 
+            help="input file containing overlapped reads from pandaseq")
     
     # parse the arguments
     args = parser.parse_args()        
 
     # do what we came here to do
-    return doWork(args)
+    doWork(args)
 
 ###############################################################################
 ###############################################################################

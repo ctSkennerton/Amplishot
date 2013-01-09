5cc21a23d12ab4ed39a1541fee60fd887ec4ad7e
ctSkennerton <c.skennerton@gmail.com>
2013-01-09 12:19:10 +1000

    Pypi-able

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup, Extension
+
+setup(
+    name='Amplishot',
+    version='0.0.1',
+    author='Connor Skennerton',
+    author_email='c.tkennerton@gmail.com',
+    packages=['amplishot', 'amplishot.test'],
+    scripts=['bin/amplishot.py'],
+    url='http://pypi.python.org/pypi/Amplishot/',
+    license='LICENSE.txt',
+    description='Amplishot',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+    ext_modules = Extension('pywumanber'['amplishot/pywumanber/wumanber_impl.c'])
+)

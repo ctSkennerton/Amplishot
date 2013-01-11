46506df4a894d949f0d41fdbdbb3957a71fc6d56
ctSkennerton <c.skennerton@gmail.com>
2013-01-11 15:51:43 +1000

    Updated the pipeline so that we now get to chimera checking

--- amplishot/phrap.py
+++ amplishot/phrap.py
@@ -114,84 +114,12 @@ class Phrap(CommandLineApplication):
 
             '-confirm_score': ValuedParameter('-', 'confirm_score', Delimiter=' '), 
 
-            '-indexwordsize': ValuedParameter('-', 'indexwordsize', Delimiter=' '), 
+            '-indexwordsize': ValuedParameter('-', 'indexwordsize', Delimiter=' ')
 
                }
     _input_handler = '_input_as_path'
     _command = 'phrap'
 
-    def __call__(self,data=None, remove_tmp=True):
-        """Run the application with the specified kwargs on data
-        
-            data: anything that can be cast into a string or written out to
-                a file. Usually either a list of things or a single string or 
-                number. input_handler will be called on this data before it 
-                is passed as part of the command-line argument, so by creating
-                your own input handlers you can customize what kind of data
-                you want your application to accept
-
-            remove_tmp: if True, removes tmp files
-        """
-        input_handler = self.InputHandler
-        suppress_stdout = self.SuppressStdout
-        suppress_stderr = self.SuppressStderr
-        if suppress_stdout:
-            outfile = FilePath('/dev/null')
-        else:
-            outfile = self.getTmpFilename(self.TmpDir)
-        if suppress_stderr:
-            errfile = FilePath('/dev/null')
-        else:
-            errfile = FilePath(self.getTmpFilename(self.TmpDir))
-        if data is None:
-            input_arg = ''
-        else:
-            input_arg = getattr(self,input_handler)(data)
-
-        # Build up the command, consisting of a BaseCommand followed by
-        # input and output (file) specifications
-        command = self._command_delimiter.join(filter(None,\
-            [str(input_arg), self.BaseCommand,'>',str(outfile),'2>',\
-                str(errfile)]))
-        if self.HaltExec: 
-            raise AssertionError, "Halted exec with command:\n" + command
-        # The return value of system is a 16-bit number containing the signal 
-        # number that killed the process, and then the exit status. 
-        # We only want to keep the exit status so do a right bitwise shift to 
-        # get rid of the signal number byte
-        exit_status = system(command) >> 8
-        
-        # Determine if error should be raised due to exit status of 
-        # appliciation
-        if not self._accept_exit_status(exit_status):
-            raise ApplicationError, \
-             'Unacceptable application exit status: %s\n' % str(exit_status) +\
-             'Command:\n%s\nStdOut:\n%s\nStdErr:\n%s\n' % (command, 
-                                                           open(outfile).read(), 
-                                                           open(errfile).read())
-        
-        # open the stdout and stderr if not being suppressed
-        out = None
-        if not suppress_stdout:
-            out = open(outfile,"r")
-        err = None        
-        if not suppress_stderr:
-            err = open(errfile,"r")
-            
-        try:
-            result = CommandLineAppResult(\
-             out,err,exit_status,result_paths=self._get_result_paths(data))
-        except ApplicationError:
-            result = self._handle_app_result_build_failure(\
-             out,err,exit_status,self._get_result_paths(data))
-
-        # Clean up the input file if one was created
-        if remove_tmp:
-            if self._input_filename:
-                remove(self._input_filename)
-                self._input_filename = None
-
-        return result
 
     def _accept_exit_status(self, exit_status):
         """Accept an exit status of 0 for the phrap program.

0e8cc764c338d87c897be456cddcb3cabcf090f6
ctSkennerton <c.skennerton@gmail.com>
2013-01-24 15:39:11 +1000

    add dependancy for biopython, remove dependancy for pycogent

--- setup.py
+++ setup.py
@@ -5,11 +5,13 @@ setup(
     version='0.0.1',
     author='Connor Skennerton',
     author_email='c.tkennerton@gmail.com',
-    packages=['amplishot', 'amplishot.test', 'amplishot.search', 'amplishot.parse'],
+    packages=['amplishot', 'amplishot.test', 'amplishot.search',
+        'amplishot.parse', 'amplishot.app'],
     scripts=['bin/amplishot'],
     url='http://pypi.python.org/pypi/Amplishot/',
     license='LICENSE.txt',
     description='Amplishot',
     long_description=open('README.md').read(),
+    requires=['biopython']
     ext_modules = [Extension('_wumanber',['amplishot/search/WuManber.cpp'])]
 )

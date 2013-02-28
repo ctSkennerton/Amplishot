6c0b61e381f16594cb3a7c96587082431f100a6a
ctSkennerton <c.skennerton@gmail.com>
2013-02-28 15:07:24 +1000

    Added in synonyms and automatically decompress output files

--- amplishot/app/fermi.py
+++ amplishot/app/fermi.py
@@ -31,34 +31,58 @@ __status__ = "Development"
 
 ###############################################################################
 import os
-from amplishot.app.util import ExtendedCommandLineApplication
-from cogent.app.util import ValuedParameter, FlagParameter, ResultPath
+from amplishot.app.util import ExtendedCommandLineApplication,\
+        CompressedResultPath
+from cogent.app.util import ValuedParameter, FlagParameter, ResultPath,\
+    ApplicationError
+import gzip
 
 class Fermi(ExtendedCommandLineApplication):
     _parameters = {
             '-c': FlagParameter('-', 'c'),
             '-P': FlagParameter('-', 'P'),
             '-B': FlagParameter('-', 'B'),
-            '-e': ValuedParameter('-', 'e', Delimeter=' ', IsPath=True),
-            '-p': ValuedParameter('-', 'p', Delimeter=' ', Value='fmdef'),
-            '-t': ValuedParameter('-', 't', Delimeter=' '),
-            '-l': ValuedParameter('-', 'l', Delimeter=' '),
-            '-k': ValuedParameter('-', 'k', Delimeter=' ')
+            '-e': ValuedParameter('-', 'e', Delimiter=' ', IsPath=True),
+            '-p': ValuedParameter('-', 'p', Delimiter=' ', Value='fmdef'),
+            '-t': ValuedParameter('-', 't', Delimiter=' '),
+            '-l': ValuedParameter('-', 'l', Delimiter=' '),
+            '-k': ValuedParameter('-', 'k', Delimiter=' ')
+            }
+    _synonyms = {
+            'paired': '-P',
+            'interleaved': '-c',
+            'split_build_indexing': '-D',
+            'original_fmd_algorithm': '-B',
+            'exec_name': '-e',
+            'threads': '-t',
+            'prefix': '-p',
+            'trim_length': '-l',
+            'kmer_length': '-k'
             }
     _command = 'run-fermi.pl'
 
     _input_handler = '_input_as_paths'
 
-    def _get_result_paths(self):
+    def _decompress_file(self, infile):
+        f_out = open(os.path.splitext(infile)[0], 'wb')
+        f_in = gzip.open(infile, 'rb')
+        f_out.writelines(f_in)
+        f_out.close()
+        f_in.close()
+        return f_out.name
+    
+    def _get_result_paths(self, data):
         results = {}
         prefix = self.Parameters['-p'].Value
-        suffixes = ['raw.fmd', 'ec.fq.gz', 'ec.fmd', 'p0.mag.gz', 'p1.mag.gz',
-                'p2.mag.gz']
         if self.Parameters['-c'].isOn() or self.Parameters['-P'].isOn():
-            suffixes.extend(['p4.fa.gz', 'p5.fq.gz'])
-            
-        for suffix in suffixes:
-            results[suffix] = ResultPath(Path=os.path.join(self.WorkingDir,
-                preifx + '.' + suffix), IsWritten=True)
-        
+            results['scaffolds'] =\
+            ResultPath(Path=self._decompress_file(os.path.join(self.WorkingDir,prefix +
+                '.p5.fq.gz')), IsWritten=True)
+
+        results['contigs'] = ResultPath(Path=self._decompress_file(os.path.join(self.WorkingDir,
+            prefix + '.p2.mag.gz')), IsWritten=True)
         return results
+
+    def _handle_app_result_build_failure(self, out, err, exit_status, result_paths):
+        raise ApplicationError('exit status: %s\n result_paths:\n%s\n' %
+                (str(exit_status), str(result_paths)))

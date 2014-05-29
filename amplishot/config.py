8459c1da7e4647abd6bb779e3e502fdad97a46c7
ctSkennerton <c.skennerton@gmail.com>
2014-05-29 00:04:35 -0700

    check for programs during startup

--- amplishot/config.py
+++ amplishot/config.py
@@ -59,7 +59,7 @@ log_level: INFO
 log_file: null
 output_directory: "."
 input_raw_reads: # path to files from the Illumina machine
-    - [null, null] # Each line contains the files for each sample. There should be two files per sample 
+    - [null, null] # Each line contains the files for each sample. There should be two files per sample
     - [null, null]
 pairtig_read_files: []
     #- null
@@ -70,10 +70,10 @@ aliases: null # names to give your files in the output.  Must be the same length
 aligner_template: null
 minimum_pairtig_length: 350 # minimum length of the overlapped pairs
 pair_overlap_length: 30 # mimimum length of the overlap
-mapper: bowtie # program used for read mapping 
-mapper_database: file.index # path to file of mapper 
+mapper: bowtie # program used for read mapping
+mapper_database: file.index # path to file of mapper
 sam_file: initial_mapping.sam
-taxonomy_file: file.tab # path to the file containing taxonomy for the reference sequences 
+taxonomy_file: file.tab # path to the file containing taxonomy for the reference sequences
 mapping_similarity_cutoffs: [0.88, 0.92, 0.94, 0.98]
 initial_mapping_similarity: 0.98 # the sequence similarity required between the reference database and the reads
 taxon_coverage: [2, 1000] # list of two numbers. The first is the minimum coverage, the second is the number of bases that need to be covered
@@ -90,6 +90,10 @@ class AmplishotConfigError(Exception):
         self.msg = msg
     def __str__(self):
         return repr(self.msg)
+
+class AmplishotProgramNotFoundError(AmplishotConfigError):
+    def __init__(self, msg=''):
+        super(AmplishotProgramNotFoundError, self).__init__(msg)
 
 class AmplishotConfig(object):
     """ Class for reading the config file
@@ -110,6 +114,51 @@ class AmplishotConfig(object):
 
     def _load(self, data):
         return yaml.load(data, Loader=Loader)
+
+    def _check_program(self, prog):
+        def is_exe(fpath):
+            return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+        fpath, fname = os.path.split(program)
+        if fpath:
+            if is_exe(program):
+                return True
+        else:
+            for path in os.environ["PATH"].split(os.pathsep):
+                path = path.strip('"')
+                exe_file = os.path.join(path, program)
+                if is_exe(exe_file):
+                    return True
+
+        raise AmplishotProgramNotFoundError('Failed to find %s in your PATH. Is it installed?' % (prog,))
+
+    def _check_required_programs(self):
+        ''' check to make sure that programs called by subprocess are present
+            in the users path before trying to run them
+        '''
+        try:
+            if self.data['otu_clustering_method'] == 'cdhit':
+                self._check_program('cd-hit')
+            else:
+                self._check_program(self.data['otu_clustering_method'])
+
+            if self.data['mapper'] == 'bowtie':
+                self._check_program('bowtie2')
+            else:
+                self._check_program(self.data['mapper'])
+
+            if self.data['assembly_method'] == 'velvet':
+                self._check_program('velveth')
+            elif self.data['assembly_method'] == 'ray':
+                self._check_program('Ray')
+            else:
+                self._check_program(self.data['assembly_method'])
+
+            self._check_program('samtools')
+            self._check_program('blastn')
+            self._check_program('pear')
+        except
+
 
     def populate_from_config_file(self, fp):
         conf_file_data = self._load(fp)
@@ -168,8 +217,6 @@ class AmplishotConfig(object):
                 self._set_path_to_absolute(self.data['taxonomy_file'])
             self.data['mapper_database'] =\
                 self._set_path_to_absolute(self.data['mapper_database'])
-            self.data['aligner_template'] =\
-                self._set_path_to_absolute(self.data['aligner_template'])
             self.data['blast_db'] =\
                 self._set_path_to_absolute(self.data['blast_db'])
         except KeyError:
@@ -178,6 +225,13 @@ class AmplishotConfig(object):
                     Please check that you have defined the nieghbours_file, \
                     taxonomy_file, mapper_database, aligner_template and \
                     blast_db and that they point to valid files')
+
+        # not compulsory files but if present fix the paths
+        try:
+            self.data['aligner_template'] =\
+                self._set_path_to_absolute(self.data['aligner_template'])
+        except KeyError:
+            pass
 
         for i in range(len(self.data['input_raw_reads'])):
             f = self.data['input_raw_reads'][i]
@@ -191,6 +245,8 @@ class AmplishotConfig(object):
                 f[1] = self._set_path_to_absolute(f[1])
             else:
                 f = self._set_path_to_absolute(f)
+
+        self._check_required_programs()
 
         return True
 ###############################################################################
